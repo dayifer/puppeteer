7108149380f44ba7af8e5bd80eeff22f473ebfc9
dayifer <dayanac.ovalle@gmail.com>
2025-01-31 15:29:07 -0400

    Tipos de Espera

---
+++ __tests__/tiposEspera.test.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer')
+
+describe('Tipos de Espera',() => {
+
+    it('test tipos de espera', async() => {
+        const browser = await puppeteer.launch({
+            headless: false, 
+            defaultViewport: null       
+        })
+
+        const page = await browser.newPage()
+        //SIRVE PARA QUE LA PAGINA TERMINE DE GURDAR LOS ELEMENTOS WAITUNTIL
+        await page.goto("https://demoqa.com/modal-dialogs/", { waitUntil:'networkidle2' }) 
+
+        await new Promise(resolve => setTimeout(resolve, 3000))
+
+        //SIRVE PARA ESPERAR POR UN SELECTOR
+
+        const button = await page.waitForSelector('#showSmallModal', { visible:true })
+        
+        // OTRA MANERA DE PASAR EL VALOR DEL SELECTOR PARA HACER UNA ACCIÓN
+        await button.click()
+       
+        
+
+        await browser.close()
+
+
+},350000)
+
+
+})
